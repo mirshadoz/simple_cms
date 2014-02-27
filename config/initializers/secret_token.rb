09678ff632240c16c845f2a473f0d596cb2326a9
mirshadoz <mirshad.oz@gmail.com>
2014-02-27 20:25:48 +0000

    Stopped at Databases and Migrations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = '1f0df7342648fe1ceea856ba41375ad911acfdc36b1235dc5ef07e758ecf5bba51ada16e851792fea12058c50c32119dddf423a4cae97c4cef62b3af014e1ccb'
